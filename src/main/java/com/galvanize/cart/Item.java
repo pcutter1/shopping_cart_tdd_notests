6c8a0ae9286bf5ecbcdc66c909f3a39b7f23b642
Paul C <pcutter@cnm.edu>
2021-04-26 16:47:14 -0600

    Changes by kiwibui

--- src/main/java/com/galvanize/cart/Item.java
+++ src/main/java/com/galvanize/cart/Item.java
@@ -1,4 +1,36 @@
 package com.galvanize.cart;
 
 public class Item {
+  private String name;
+  private double price;
+  private boolean onSale;
+//  private int quantity;
+
+  public Item(String name, double price, boolean onSale) {
+    this.name = name;
+    this.price = price;
+    this.onSale = onSale;
+//    quantity = 1;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public boolean isOnSale() {
+    return onSale;
+  }
+
+  //  public void increaseQuantity() {
+//    quantity++;
+//  }
+//
+//  public int getQuantity() {
+//    return quantity;
+//  }
+
 }
