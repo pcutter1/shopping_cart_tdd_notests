5e1236bae9410e22a936cfa0d8fac122457f3ba6
Paul C <pcutter@cnm.edu>
2021-04-26 16:47:09 -0600

    Passing all tests

--- src/main/java/com/galvanize/cart/Cart.java
+++ src/main/java/com/galvanize/cart/Cart.java
@@ -1,4 +1,66 @@
 package com.galvanize.cart;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
 public class Cart {
+
+  private double totalPrice;
+  private List<Item> items = new ArrayList<>();
+  private List<Item> onSaleItems = new ArrayList<>();
+  private HashMap<String, Integer> quantities = new HashMap<>();
+
+
+  public Cart() {
+    totalPrice = 0.00;
+  }
+
+  public List<Item> getItems() {
+    return items;
+  }
+
+  public double getTotalPrice() {
+    return totalPrice;
+  }
+
+  public void addItem(Item item) {
+//    if (items.contains(item)) {
+//      System.out.println("This block runs");
+//      int idx = items.indexOf(item);
+//      items.get(idx).increaseQuantity();
+//    }
+    if (quantities.containsKey(item.getName())) {
+      quantities.put(item.getName(), quantities.get(item.getName()) + 1);
+    } else {
+      quantities.put(item.getName(), 1);
+    }
+
+    items.add(item);
+    if (item.isOnSale()) {
+      onSaleItems.add(item);
+    }
+    totalPrice += item.getPrice();
+  }
+
+  public List<Item> itemizedList() {
+    return items;
+  }
+
+  public int itemQuantities() {
+    int total = 0;
+
+    for (String key : quantities.keySet()) {
+      total += quantities.get(key);
+    }
+
+    return total;
+  }
+
+
+  public List<Item> onSaleItems() {
+    return onSaleItems;
+  }
+
 }
--- src/test/java/com/galvanize/cart/CartTest.java
+++ src/test/java/com/galvanize/cart/CartTest.java
@@ -12,5 +12,118 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class CartTest {
 
+  @Test
+  public void testEmptyCart() {
+    Cart cartUnderTest = new Cart();
+
+    int expected = 0;
+    int actual = cartUnderTest.getItems().size();
+
+    assertEquals(expected, actual, "Testing if cart is empty");
+  }
+
+  @Test
+  public void testEmptyCartBalance() {
+    Cart cartUnderTest = new Cart();
+
+    double expected = 0.00;
+    double actual = cartUnderTest.getTotalPrice();
+
+    assertEquals(expected, actual, "Testing if empty cart has zero balance");
+  }
+
+  @Test
+  public void testAddItem() {
+    Cart cartUnderTest = new Cart();
+    Item firstItem = new Item("Rolex", 230.00, false);
+    cartUnderTest.addItem(firstItem);
+
+    int expected = 1;
+    int actual = cartUnderTest.getItems().size();
+
+    assertEquals(expected, actual, "Testing addItem method");
+  }
+
+  @Test
+  public void testTotalPrice() {
+    Cart cartUnderTest = new Cart();
+    Item firstItem = new Item("Rolex", 230.00, false);
+
+    cartUnderTest.addItem(firstItem);
+
+    double expected = 230;
+    double actual = cartUnderTest.getTotalPrice();
+
+    assertEquals(expected, actual, "Testing getTotalPrice method");
+  }
+
+  @Test
+  public void testItemizedList() {
+    Cart cartUnderTest = new Cart();
+    Item firstItem = new Item("Rolex", 230.00, false);
+
+    cartUnderTest.addItem(firstItem);
+
+    List<Item> expected = new ArrayList<>();
+    expected.add(firstItem);
+
+    List<Item> actual = cartUnderTest.itemizedList();
+
+    assertEquals(expected, actual, "Testing itemizedList method");
+  }
+
+  @Test
+  public void testMultipleTotalPrice() {
+    Cart cartUnderTest = new Cart();
+
+    Item firstItem = new Item("Rolex", 230.00, false);
+    Item secondItem = new Item("Rolex", 230.00, false);
+
+    cartUnderTest.addItem(firstItem);
+    cartUnderTest.addItem(secondItem);
+
+    double expected = 460.00;
+    double actual = cartUnderTest.getTotalPrice();
+
+    assertEquals(expected, actual, "Testing total price method for multiple item");
+  }
+
+  @Test
+  public void testItemQuantity() {
+    Cart cartUnderTest = new Cart();
+
+    Item firstItem = new Item("Rolex", 230.00, false);
+    Item secondItem = new Item("Rolex", 230.00, false);
+    Item thirdItem = new Item("Rolex", 230.00, false);
+
+    cartUnderTest.addItem(firstItem);
+    cartUnderTest.addItem(secondItem);
+    cartUnderTest.addItem(thirdItem);
+
+    int expected = 3;
+    int actual = cartUnderTest.itemQuantities();
+
+    assertEquals(expected, actual, "Testing itemQuantities method");
+  }
+
+  @Test
+  public void testOnSaleItems() {
+    Cart cartUnderTest = new Cart();
+
+    Item firstItem = new Item("Rolex", 230.00, false);
+    Item secondItem = new Item("Rolex", 150.00, true);
+    Item thirdItem = new Item("Rolex", 230.00, false);
+
+    cartUnderTest.addItem(firstItem);
+    cartUnderTest.addItem(secondItem);
+    cartUnderTest.addItem(thirdItem);
+
+    List<Item> expected = new ArrayList<>();
+    expected.add(secondItem);
+
+    List<Item> actual = cartUnderTest.onSaleItems();
+
+    assertEquals(expected, actual, "Testing onSaleItems method");
+  }
 
 }
